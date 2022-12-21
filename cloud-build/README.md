5b30d005464db50355ceae1673d1d11d421586ce
deepjy <deepjyoti.saha007@gmail.com>
2022-12-21 23:27:12 +0530

    Adding files for Cloud functions and CICD

---
+++ cloud-build/README.md
@@ -0,0 +1,13 @@
+# CICD in GCP
+The following steps will be performed to deploy the Weather app via CICD process in GCP.
+
+### Components used - 
+- CSR
+- Cloud Build
+- Cloud Run
+
+
+### Steps
+- Create a repository in CSR
+- Upload the code and Dockerfile
+- Create Cloud Build process
---
+++ cloud-functions/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template_string
+
+
+def hello_world(request):
+    if request.method == 'POST':
+        vals = request.form.getlist('numbers')
+        A = float(vals[0])
+        B = float(vals[1])
+        C = float(vals[2])
+        #result = round((math.sqrt(abs(A))+ B ** 3) + C ** (1. /3), 3)
+        result = (A+B+C)
+    else:
+        A = ''
+        B = ''
+        C = ''
+        result = ''
+    return render_template_string('''
+<html
+<body style="background-color:greenyellow;">    
+<b>Calculator App to sum 3 numbers </b> .<br>
+<p>Powered by - <a> Cloud Function </a></p>
+<form  method="POST">
+    Please input 3 numbers - A,B,C:
+    <p>Number A: <input type="text" size = "6" name="numbers" value="{{ v_A }}"></p>
+    <p>Number B: <input type="text" size = "6" name="numbers" value="{{ v_B }}"></p>
+    <p>Number C: <input type="text" size = "6" name="numbers" value="{{ v_C }}"></p>
+    <input type="submit" value="Click here to compute">
+    <p>Result: <input type="text" size = "6" name="result" value="{{ v_result }}"></p>
+</form>
+</html>
+''', v_A=A, v_B=B, v_C=C, v_result=result)
